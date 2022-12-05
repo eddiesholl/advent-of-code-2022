4517975b1ef252720020e79734031003b224cd7e
Eddie Sholl <eddie.sholl@gmail.com>
2022-12-05 14:48:45 +1100

    refactor: split into funcs

--- puzzles/01/index.ts
+++ puzzles/01/index.ts
@@ -9,31 +9,39 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const file = await open(path.join(__dirname, "input.txt"));
 
-const inputLines = fs
-  .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
-  .split(/\r?\n/);
+const inputPath = path.join(__dirname, "input.txt");
+type MaxElf = {
+  number: Number;
+  calorieCount: Number;
+};
+const readLines = () => fs.readFileSync(inputPath, "utf-8").split(/\r?\n/);
+const findMaxElf = (lines: string[]): MaxElf => {
+  let currentElf = 1;
+  let maxElfNumber = 1;
+  let maxElfCalorieCount = 0;
+  let currentElfCalories = 0;
+  for (const line of lines) {
+    if (line === "") {
+      // if the line is blank, move to the next elf
+      currentElf++;
+      currentElfCalories = 0;
+    } else {
+      // else update the current elf calorie total
+      const currentCalorieValue = parseInt(line);
+      currentElfCalories += currentCalorieValue;
 
-let currentElf = 1;
-let maxElfNumber = 1;
-let maxElfCalorieCount = 0;
-let currentElfCalories = 0;
-for (const line of inputLines) {
-  if (line === "") {
-    // if the line is blank, move to the next elf
-    currentElf++;
-    currentElfCalories = 0;
-  } else {
-    // else update the current elf calorie total
-    const currentCalorieValue = parseInt(line);
-    currentElfCalories += currentCalorieValue;
-
-    if (currentElfCalories > maxElfCalorieCount) {
-      maxElfNumber = currentElf;
-      maxElfCalorieCount = currentElfCalories;
-      console.log(currentCalorieValue);
+      if (currentElfCalories > maxElfCalorieCount) {
+        maxElfNumber = currentElf;
+        maxElfCalorieCount = currentElfCalories;
+      }
     }
   }
-}
+  return {
+    number: maxElfNumber,
+    calorieCount: maxElfCalorieCount,
+  };
+};
+const displayElf = (elf: MaxElf) =>
+  console.log(`Elf ${elf.number} has the most calories, ${elf.calorieCount}`);
 
-console.log(maxElfNumber);
-console.log(maxElfCalorieCount);
+displayElf(findMaxElf(readLines()));
