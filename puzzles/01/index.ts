16cb45b8dc86f21d670a05b4246e7bd5b74d5535
Eddie Sholl <eddie.sholl@gmail.com>
2022-12-05 13:08:20 +1100

    feat: basic script running via ts-node

--- puzzles/01/index.ts
+++ puzzles/01/index.ts
@@ -1,1 +1,21 @@
-console.log('hey')
+import { open } from "node:fs/promises";
+import path from "path"
+import fs from 'fs';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const file = await open(path.join(__dirname, "input.txt"));
+
+let c = 0;
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), 'utf-8').split(/\r?\n/)
+console.log(input.length)
+for await (const line of input) {
+// for await (const line of file.readLines()) {
+  c++;
+  if (c < 5) {
+    console.log(line);
+  }
+}
