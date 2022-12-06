55d59386462b593ee6c4d5b783a3d807422535b7
Eddie Sholl <eddie.sholl@gmail.com>
2022-12-06 21:24:14 +1100

    feat: tidy up reduce

--- puzzles/01/index.ts
+++ puzzles/01/index.ts
@@ -1,25 +1,17 @@
-import { open } from "node:fs/promises";
 import path from "path";
 import fs from "fs";
 
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-// const file = await open(path.join(__dirname, "input.txt"));
-
 const inputPath = path.join(__dirname, "input.txt");
-type MaxElf = {
+interface MaxElf {
   number: Number;
   calorieCount: Number;
-};
+}
+
 const readLines = () => fs.readFileSync(inputPath, "utf-8").split(/\r?\n/);
 const findMaxElf = (lines: string[]): MaxElf => {
   const result = lines.reduce(
     (prev, curr) => {
       if (curr === "") {
-        console.log("empty line");
         return {
           ...prev,
           currentElfNumber: prev.currentElfNumber + 1,
@@ -29,18 +21,14 @@ const findMaxElf = (lines: string[]): MaxElf => {
         const currentCalorieValue = parseInt(curr);
         const currentElfCalories = prev.currentCalories + currentCalorieValue;
 
-        console.log(currentElfCalories);
-        console.log(prev.maxCalories);
-        if (currentElfCalories > prev.maxCalories) {
-          console.log("new max");
+        if (currentElfCalories > prev.calorieCount) {
           return {
             ...prev,
-            maxElfNumber: prev.currentElfNumber,
+            number: prev.currentElfNumber,
             currentCalories: currentElfCalories,
-            maxCalories: currentElfCalories,
+            calorieCount: currentElfCalories,
           };
         } else {
-          console.log("keep max");
           return {
             ...prev,
             currentCalories: currentElfCalories,
@@ -50,15 +38,12 @@ const findMaxElf = (lines: string[]): MaxElf => {
     },
     {
       currentElfNumber: 1,
-      maxElfNumber: 1,
+      number: 1,
       currentCalories: 0,
-      maxCalories: 0,
+      calorieCount: 0,
     }
   );
-  return {
-    number: result.maxElfNumber,
-    calorieCount: result.maxCalories,
-  };
+  return { number: result.number, calorieCount: result.calorieCount };
 };
 const displayElf = (elf: MaxElf) =>
   console.log(`Elf ${elf.number} has the most calories, ${elf.calorieCount}`);
