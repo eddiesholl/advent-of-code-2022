e17f210dc829351f174cfb5221e2f4d53ca44278
Eddie Sholl <eddie.sholl@pathzero.com>
2022-12-07 21:35:24 +1100

    feat: basic funcs passing tests

--- puzzles/03/index.test.ts
+++ puzzles/03/index.test.ts
@@ -1,4 +1,4 @@
-import { a } from "./index";
+import { a, findIntersectionIndex, scorePack, slicePack } from "./index";
 
 describe("03", () => {
   describe("a", () => {
@@ -6,6 +6,54 @@ describe("03", () => {
       it("handles no lines", () => {
         expect(a([])).toEqual(0);
       });
+
+      it("handles no overlap", () => {
+        expect(a(["ab"])).toEqual(0);
+      });
+
+      it("handles overlapping a", () => {
+        expect(a(["aa"])).toEqual(1);
+      });
+      it("handles more complex overlapping b", () => {
+        expect(a(["abcb"])).toEqual(2);
+      });
+    });
+  });
+
+  describe("findIntersectionIndex", () => {
+    it("handles empty arrays", () => {
+      expect(findIntersectionIndex([], [])).toEqual([]);
+    });
+
+    it("handles a simple overlap", () => {
+      expect(findIntersectionIndex(["a"], ["a"])).toEqual([0]);
+    });
+  });
+
+  describe("slicePack", () => {
+    it("slices an empty string", () => {
+      expect(slicePack("")).toStrictEqual(["", ""]);
+    });
+    it("slices a simple pack", () => {
+      expect(slicePack("ab")).toStrictEqual(["a", "b"]);
+    });
+
+    it("slices a more interesting pack", () => {
+      expect(slicePack("abcd")).toStrictEqual(["ab", "cd"]);
+    });
+
+    it("handles odd counts", () => {
+      expect(slicePack("abc")).toStrictEqual(["ab", "c"]);
+    });
+  });
+
+  describe("scorePack", () => {
+    it("scores a simple overlap", () => {
+      expect(scorePack("abcb")).toEqual(2);
+    });
+
+    it.only("scores a capital overlap", () => {
+      expect(scorePack("ZbcZ")).toEqual(52);
     });
   });
 });
--- puzzles/03/index.ts
+++ puzzles/03/index.ts
@@ -1,8 +1,43 @@
 import { sumValues } from "../common/math";
 
-const a = (lines: string[]) => lines.map(findIntersection).reduce(sumValues, 0);
+const a = (lines: string[]) => lines.map(scorePack).reduce(sumValues, 0);
 const b = () => void 0;
-const findIntersection = (line: string): number => {
-  return 0;
+const findIntersectionIndex = (
+  arr1: string[],
+  arr2: string[]
+): (number | undefined)[] =>
+  arr1
+    .map((c, ix) => {
+      if (arr2.includes(c)) {
+        console.log(ix);
+        return ix;
+      }
+    })
+    .filter((ix) => ix !== undefined);
+
+const slicePack = (line: string): string[] => [
+  line.slice(0, Math.ceil(line.length / 2)),
+  line.slice(Math.ceil(line.length / 2)),
+];
+const scoreChar = (str: string, at: number): number => {
+  const charCode = str.charCodeAt(at);
+  if (charCode > 96) {
+    return charCode - 96;
+  } else {
+    return charCode - 38;
+  }
 };
-export { a, b };
+const scorePack = (line: string): number => {
+  const [str1, str2] = slicePack(line);
+  const arr1 = Array.from(str1);
+  const arr2 = Array.from(str2);
+
+  console.log(str1);
+  console.log(str2);
+  const intersections = findIntersectionIndex(arr1, arr2);
+  console.log(intersections);
+  return intersections.length === 1 && intersections[0] !== undefined
+    ? scoreChar(str1, intersections[0])
+    : 0;
+};
+export { a, b, findIntersectionIndex, scorePack, slicePack };
