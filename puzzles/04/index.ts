0c58e8b391366a2fa9a3969cf6db5028600c5709
Eddie Sholl <eddie.sholl@pathzero.com>
2022-12-07 22:20:31 +1100

    chore: tidy logs

--- puzzles/04/index.ts
+++ puzzles/04/index.ts
@@ -12,14 +12,11 @@ const rangeContains = (rLarger: Range, rSmaller: Range): boolean => {
 };
 const rangeLineContained = (line: string): boolean => {
   const rangeStrs = line.split(",");
-  console.log(rangeStrs);
   if (rangeStrs.length !== 2) {
     return false;
   }
   const r1 = parseRange(rangeStrs[0]);
   const r2 = parseRange(rangeStrs[1]);
-  console.log(r1);
-  console.log(r2);
   const r1Size = r1.end - r1.start;
   const r2Size = r2.end - r2.start;
   return r1Size > r2Size ? rangeContains(r1, r2) : rangeContains(r2, r1);
