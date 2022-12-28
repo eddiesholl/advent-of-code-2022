05650af180245b0a00c0f54e0f19a0af6591c8dd
Eddie Sholl <eddie.sholl@pathzero.com>
2022-12-28 22:08:22 +1100

    feat: 21 passing

--- puzzles/21/run.ts
+++ puzzles/21/run.ts
@@ -1,5 +1,5 @@
 import { readLines } from "../common/input";
-import { a } from "./index";
+import { findRootValue, parseLines } from "./index";
 
-const score = a(readLines(__dirname));
+const score = findRootValue(parseLines(readLines(__dirname)));
 console.log("Score is " + score);
