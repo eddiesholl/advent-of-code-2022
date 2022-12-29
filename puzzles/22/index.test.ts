81a06f7d277789bb7ec6a4494e95cfb6b8e646bc
Eddie Sholl <eddie.sholl@pathzero.com>
2022-12-29 22:16:41 +1100

    feat: example for 22 passing

--- puzzles/22/index.test.ts
+++ puzzles/22/index.test.ts
@@ -1,8 +1,91 @@
 import { readLines } from "../common/input";
-import { calculateScore, processMoves } from "./index";
+import { applyWalk, calculateScore, Grid, processMoves } from "./index";
 import { parseLines } from "./parse";
 
 describe("22", () => {
+  let grid: Grid;
+  beforeEach(() => {
+    grid = [
+      //-----------------------y
+      [" ", ".", ".", "#"], // 0
+      [" ", ".", ".", "#"], // 1
+      [".", ".", ".", "#"], // 2
+      [".", ".", ".", "."], // 3
+      //0    1    2    3
+    ];
+  });
+  describe("applyWalk", () => {
+    describe("simple open moves", () => {
+      it("can move right", () => {
+        expect(applyWalk({ x: 1, y: 0, bearing: "right" }, grid, 1)).toEqual({
+          x: 2,
+          y: 0,
+          bearing: "right",
+        });
+      });
+      it("can move left", () => {
+        expect(applyWalk({ x: 2, y: 0, bearing: "left" }, grid, 1)).toEqual({
+          x: 1,
+          y: 0,
+          bearing: "left",
+        });
+      });
+      it("can move up", () => {
+        expect(applyWalk({ x: 1, y: 2, bearing: "up" }, grid, 2)).toEqual({
+          x: 1,
+          y: 0,
+          bearing: "up",
+        });
+      });
+      it("can move down", () => {
+        expect(applyWalk({ x: 2, y: 0, bearing: "down" }, grid, 2)).toEqual({
+          x: 2,
+          y: 2,
+          bearing: "down",
+        });
+      });
+    });
+    describe("blocked by a wall", () => {
+      it("stops at a simple wall", () => {
+        expect(applyWalk({ x: 1, y: 0, bearing: "right" }, grid, 5)).toEqual({
+          x: 2,
+          y: 0,
+          bearing: "right",
+        });
+      });
+      it("stops if the wall is past a wrap", () => {});
+    });
+    describe("line wrappings", () => {
+      it("wraps bottom to top", () => {
+        expect(applyWalk({ x: 2, y: 3, bearing: "down" }, grid, 2)).toEqual({
+          x: 2,
+          y: 1,
+          bearing: "down",
+        });
+      });
+      it("wraps top to bottom", () => {
+        expect(applyWalk({ x: 1, y: 0, bearing: "up" }, grid, 1)).toEqual({
+          x: 1,
+          y: 3,
+          bearing: "up",
+        });
+      });
+      it("wraps left to right", () => {
+        expect(applyWalk({ x: 0, y: 3, bearing: "left" }, grid, 1)).toEqual({
+          x: 3,
+          y: 3,
+          bearing: "left",
+        });
+      });
+      it("wraps through closed cells", () => {
+        expect(applyWalk({ x: 0, y: 2, bearing: "up" }, grid, 1)).toEqual({
+          x: 0,
+          y: 3,
+          bearing: "up",
+        });
+      });
+    });
+  });
   describe("processMoves", () => {
     it("handles the example", () => {
       expect(
--- puzzles/22/index.ts
+++ puzzles/22/index.ts
@@ -33,14 +33,123 @@ const moveMap: Record<string, MoveShifts> = {
 const applyTurn = (l: Location, turn: string): Location => {
   return { ...l, bearing: moveMap[l.bearing][turn] };
 };
-
+const getCandidateLoc = (l: Location): Location => {
+  const x =
+    l.bearing === "right" ? l.x + 1 : l.bearing === "left" ? l.x - 1 : l.x;
+  const y = l.bearing === "down" ? l.y + 1 : l.bearing === "up" ? l.y - 1 : l.y;
+  return { x, y, bearing: l.bearing };
+};
+const firstCellInColumn = (
+  x: number,
+  bearing: Bearing,
+  grid: Grid
+): Location | null => {
+  let y = bearing === "up" ? grid.length - 1 : 0;
+  while (y >= 0 && y < grid.length) {
+    const m = grid[y][x];
+    if (m === "#") {
+      return null;
+    } else if (m === ".") {
+      return { x, y, bearing };
+    }
+    y += bearing === "up" ? -1 : 1;
+  }
+  return null;
+};
+const firstCellInRow = (
+  y: number,
+  bearing: Bearing,
+  grid: Grid
+): Location | null => {
+  const row = grid[y];
+  let x = bearing === "left" ? row.length - 1 : 0;
+  while (x >= 0 && x < row.length) {
+    const m = grid[y][x];
+    if (m === "#") {
+      return null;
+    } else if (m === ".") {
+      return { x, y, bearing };
+    }
+    x += bearing === "left" ? -1 : 1;
+  }
+  return null;
+};
+const applyWalk = (
+  startLoc: Location,
+  grid: Grid,
+  distance: number
+): Location => {
+  let newLoc = { ...startLoc };
+  let d = 0;
+  while (d < distance) {
+    const candidateLoc = getCandidateLoc(newLoc);
+    const candidateRow = grid[candidateLoc.y];
+    if (candidateRow) {
+      const candidateCell = candidateRow[candidateLoc.x];
+      if (candidateCell) {
+        if (candidateCell === "#") {
+          break;
+        } else if (candidateCell === ".") {
+          newLoc = candidateLoc;
+        } else {
+          // We've struck a closed cell, and need to wrap
+          if (newLoc.bearing === "left" || newLoc.bearing === "right") {
+            const candidateLoc = firstCellInRow(newLoc.y, newLoc.bearing, grid);
+            if (candidateLoc) {
+              newLoc = candidateLoc;
+            } else {
+              // console.log("walk bail, no h wrap candidate after close");
+              break;
+            }
+          } else {
+            const candidateLoc = firstCellInColumn(
+              newLoc.x,
+              newLoc.bearing,
+              grid
+            );
+            if (candidateLoc) {
+              newLoc = candidateLoc;
+            } else {
+              // console.log("walk bail, no v wrap candidate after close");
+              break;
+            }
+          }
+        }
+      } else {
+        // console.log("wrap h");
+        // We must have wrapped horizontally. Check if its possible to wrap in this current row
+        const candidateLoc = firstCellInRow(newLoc.y, newLoc.bearing, grid);
+        // console.log(candidateLoc);
+        if (candidateLoc) {
+          newLoc = candidateLoc;
+        } else {
+          // console.log("walk bail, no h wrap candidate after end");
+          break;
+        }
+      }
+    } else {
+      // We must have wrapped vertically. Check if it's possible to wrap in this current column
+      const candidateLoc = firstCellInColumn(newLoc.x, newLoc.bearing, grid);
+      if (candidateLoc) {
+        newLoc = candidateLoc;
+      } else {
+        // console.log("walk bail, no v wrap candidate after end");
+        break;
+      }
+    }
+    d++;
+  }
+  // console.log("d: " + d);
+  return newLoc;
+};
 const processMoves = (game: Game): Location => {
   let location = findStart(game);
   game.moves.forEach((move) => {
     if (isTurn(move)) {
       location = applyTurn(location, move);
+    } else {
+      location = applyWalk(location, game.grid, move);
     }
-    // TODO: walk
   });
 
   return location;
@@ -56,4 +165,4 @@ const calculateScore = (l: Location) => {
       : 3;
   return 4 * (l.x + 1) + 1000 * (l.y + 1) + bearingScore;
 };
-export { Game, Grid, Move, Material, processMoves, calculateScore };
+export { Game, Grid, Move, Material, processMoves, calculateScore, applyWalk };
