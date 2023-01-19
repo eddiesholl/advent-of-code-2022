34ef8b7c6f69db576c6e533de1d4c95a315589f5
Eddie Sholl <eddie.sholl@pathzero.com>
2023-01-19 15:58:43 +1100

    feat: 19 passing

--- puzzles/19/run.ts
+++ puzzles/19/run.ts
@@ -1,7 +1,5 @@
 import { readLines } from "../common/input";
 import { parseLines, processBlueprints } from "./index";
 
-const score = processBlueprints(
-  parseLines(readLines(__dirname, "example.txt"))
-);
+const score = processBlueprints(parseLines(readLines(__dirname, "input.txt")));
 console.log("Score is " + score);
